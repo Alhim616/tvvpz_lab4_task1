75a919a301562316a829c42c474ca512d1fa20ec
Yanushevych Dmytro <alhim9dima@gmail.com>
2026-03-22 02:21:20 +0200

    Fix code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from calculator import add, subtract
 
 if __name__ == "__main__":
-    print("2 + 3 =", add(2,3))
-    print("5 - 2 =", subtract(5,2))
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
